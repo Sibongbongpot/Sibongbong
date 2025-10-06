7ce31113de12a032956f1e1b31bc03088d8a40d3
hyeonseo <hsahn0303@gmail.com>
2025-10-06 19:52:12 +0900

    commit

---
+++ backend/src/main/java/com/SibongbongPot/Sibongbong_backend/SibongbongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SibongbongPot.Sibongbong_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibongbongBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibongbongBackendApplication.class, args);
+	}
+
+}
